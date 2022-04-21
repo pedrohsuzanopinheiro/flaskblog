a34a57e5eec2fdbcba881241e9fb2ffb55afd36b
Pedro Pinheiro <pedro.hsuzanopinheiro@gmail.com>
2022-04-21 10:26:20 -0300

    creating .env file and configuring flask to use those variables

---
+++ README.md
@@ -0,0 +1,11 @@
+**You'll need a .env file in root directory with the following:**
+
+```bash
+SECRET_KEY=
+SQLALCHEMY_DATABASE_URI=
+MAIL_SERVER=
+MAIL_PORT=
+MAIL_USE_TLS=
+MAIL_USERNAME=
+MAIL_PASSWORD=
+```
--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,11 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class Config:
-    SECRET_KEY = (
-        "009ed827052896fc6ae7426c140ea46d"  # TODO: Use .env to hide those files.
-    )
-    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Supressing notifications
-    MAIL_SERVER = "smtp.googlemail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = ""
-    MAIL_PASSWORD = ""
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_PORT = os.getenv("MAIL_PORT")
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS")
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
